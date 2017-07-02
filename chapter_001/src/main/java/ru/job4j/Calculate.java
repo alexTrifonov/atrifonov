46e2064e4959958f1fcde39fcf81311a26a47e3c
Alexandr <nudniy.slava@yandex.ru>
2017-07-02 23:15:07 +0500

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,11 @@
 package ru.job4j;
 
+/**
+ * Class Класс для вывода "Hello world" 
+ * @autor atrifonov
+ * @since 02.07.2017
+ * @version 2
+ */ 
 public class Calculate{
 	public static void main(String[] args) {
 		System.out.println("Hello world");
