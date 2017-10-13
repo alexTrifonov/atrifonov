c2c66668a033cae3725dc4742da1667dbb3b299f
Alexandr <nudniy.slava@yandex.ru>
2017-10-13 20:52:22 +0500

    ed task 1099

--- chapter_007/src/main/java/ru/job4j/taskBlockCondition/SThreadPool.java
+++ chapter_007/src/main/java/ru/job4j/taskBlockCondition/SThreadPool.java
@@ -1,8 +1,5 @@
 package ru.job4j.taskBlockCondition;
 
-import com.sun.istack.internal.NotNull;
-
-import java.util.Arrays;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -17,19 +14,13 @@ public class SThreadPool {
      * Thread safe queue for adding and getting objects Runnable.
      */
     private final BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>();
-    /**
-     * Array threads for execute tasks from queue.
-     */
-    private final MyThread[] threads;
 
     /**
      * Constructs object SThreadPool. In this object count of threads in array equal count of CPU on computer.
      */
     public SThreadPool() {
-        threads = new MyThread[Runtime.getRuntime().availableProcessors()];
-        for(MyThread t : threads) {
-            t = new MyThread();
-            t.start();
+        for(int i = 0; i < Runtime.getRuntime().availableProcessors(); i++) {
+            new SThread().start();
         }
     }
 
@@ -50,14 +41,12 @@ public class SThreadPool {
     /**
      * Nested class is descendent of class Thread. Class for array threads.
      */
-    private class MyThread extends Thread {
+    private class SThread extends Thread {
         @Override
         public void run() {
-            Runnable r;
             while (!this.isInterrupted()) {
                 try {
-                    r = queue.take();
-                    r.run();
+                    queue.take().run();
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                 } catch (RuntimeException e) {
@@ -66,5 +55,4 @@ public class SThreadPool {
             }
         }
     }
-
 }
