4426d9a27ec97fa80a80549bbcdf2b74ee00fc4c
alexTrifonov <nudniy.slava@yandex.ru>
2017-12-15 21:02:56 +0500

    task 2514

---
+++ chapter_009/src/main/java/ru/job4j/servlets/EchoServlet.java
@@ -0,0 +1,29 @@
+package ru.job4j.servlets;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Simple servlet.
+ */
+public class EchoServlet extends HttpServlet {
+    /**
+     * Log4j logger.
+     */
+    private static final Logger LOG_SERVLET = LogManager.getLogger(EchoServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.append("hello world");
+        writer.flush();
+    }
+}
---
+++ chapter_009/src/main/java/ru/job4j/servlets/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for simple web application - "hello world".
+ * @author atrifonov.
+ * @since 15.12.2017.
+ * @version 1.
+ */
+package ru.job4j.servlets;
