90540afd0c50b8fc0dd82de8e4a49564c9d149b6
alexTrifonov <nudniy.slava@yandex.ru>
2018-02-25 18:26:04 +0500

    edit task 862

--- chapter_003_IO/src/main/java/ru/job4j/simpleIO/ChatConsole.java
+++ chapter_003_IO/src/main/java/ru/job4j/simpleIO/ChatConsole.java
@@ -18,41 +18,47 @@ public class ChatConsole {
      * Logger this chat.
      */
     private static final Logger CHAT_CONSOLE = LogManager.getLogger(ChatConsole.class);
+    private final String STOP = "стоп";
+    private final String CONTINUE = "продолжить";
+    private final String FINISH = "закончить";
 
     /**
      * Make chat with console.
      * @throws IOException IOException.
      */
-    public void chat() throws IOException {
-        File fileAnsw = new File("chapter_003_IO\\src\\main\\resources\\ru\\job4j\\simpleIO\\Answer.txt");
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(fileAnsw), "UTF-8"));
-        ArrayList<String> listAnsw = new ArrayList<>();
-        String line;
-        while ((line = br.readLine()) != null) {
-            listAnsw.add(line);
+    public void chat() {
+        File fileAnsw = new File(".\\Answer.txt");
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(fileAnsw), "UTF-8"))) {
+            ArrayList<String> listAnsw = new ArrayList<>();
+            String line;
+            while ((line = br.readLine()) != null) {
+                listAnsw.add(line);
+            }
+
+            BufferedReader consoleIn = new BufferedReader(new InputStreamReader(System.in));
+            CHAT_CONSOLE.info("Начало абсурда - разговоры с консолью");
+            String message = consoleIn.readLine();
+            CHAT_CONSOLE.info(String.format("user : %s", message));
+            int size = listAnsw.size();
+            int numb;
+            boolean stop = false;
+            while (!FINISH.equals(message)) {
+                if (CONTINUE.equals(message)) {
+                    stop = false;
+                } else if (STOP.equals(message)) {
+                    stop = true;
+                } else if (!stop) {
+                    numb = new Random().nextInt(size);
+                    System.out.println(listAnsw.get(numb));
+                    CHAT_CONSOLE.info(String.format("console : %s", listAnsw.get(numb)));
+                }
+                message = consoleIn.readLine();
+                CHAT_CONSOLE.info(String.format("user : %s", message));
+            }
+            CHAT_CONSOLE.info("Вот и пообщались.");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        br.close();
-        BufferedReader consoleIn = new BufferedReader(new InputStreamReader(System.in));
-        CHAT_CONSOLE.info("Начало абсурда - разговоры с консолью");
-        String message = consoleIn.readLine();
-        CHAT_CONSOLE.info(String.format("user : %s", message));
-        int size = listAnsw.size();
-        int numb;
-        boolean stop = false;
-        while (!message.equals("закончить")) {
-            if (message.equals("продолжить")) {
-                stop = false;
-            } else if (message.equals("стоп")) {
-                stop = true;
-            } else if (!stop) {
-                numb = new Random().nextInt(size);
-                System.out.println(listAnsw.get(numb));
-                CHAT_CONSOLE.info(String.format("console : %s", listAnsw.get(numb)));
-            }
-            message = consoleIn.readLine();
-            CHAT_CONSOLE.info(String.format("user : %s", message));
-        }
-        CHAT_CONSOLE.info("Вот и пообщались.");
     }
 }
